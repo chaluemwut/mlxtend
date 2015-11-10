2778746a6c813f54cf409f2268eaaed2eeb41736
chaluemwut <chaluemwut@hotmail.com>
2015-11-10 12:01:29 +0700

    add chaluemwut folder

---
+++ chaluemwut/main.py
@@ -0,0 +1,11 @@
+'''
+Created on Nov 10, 2558 BE
+
+@author: ECP
+original : http://sebastianraschka.com/Articles/2014_sequential_sel_algos.html
+git : https://github.com/rasbt/mlxtend#sequential-backward-selection
+
+'''
+
+if __name__ == '__main__':
+    pass
