e8e7690cd12670f529eb674802c9e07d766e044a
chaluemwut <chaluemwut@hotmail.com>
2015-11-10 12:38:57 +0700

    test call mlxtend

--- chaluemwut/main.py
+++ chaluemwut/main.py
@@ -6,6 +6,19 @@ original : http://sebastianraschka.com/Articles/2014_sequential_sel_algos.html
 git : https://github.com/rasbt/mlxtend#sequential-backward-selection
 
 '''
+from sklearn.tree import DecisionTreeClassifier
+from mlxtend.feature_selection import SBS
+from sklearn.datasets import load_iris
 
+def test_hello():
+    print 'test'
+    iris = load_iris()
+    x, y = iris.data, iris.target
+    print x[0]
+    clf = DecisionTreeClassifier()
+    sbs = SBS(clf, k_features=2)
+    sbs = sbs.fit(x, y)
+    print sbs.transform(x)
+            
 if __name__ == '__main__':
-    pass
+    test_hello()
